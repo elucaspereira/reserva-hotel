3855adf70ccd249b91d5c55d865413e12bc052e1
Lucas Pereira <lucas_rm35@hotmail.com>
2024-02-12 11:29:20 -0400

    criando usuarios

---
+++ src/auth/User.js
@@ -0,0 +1,13 @@
+const {v4:uuidv4}= require("uuid")
+class User{
+    constructor({id, name, userName, document, email, password}){
+        this.id = id ?? uuidv4();
+        this.name = name;
+        this.userName = userName;
+        this.document = document;
+        this.email = email;
+        this.password = password;
+        
+    }
+}
+module.exports = User;
